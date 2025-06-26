c479e378d77143361a60c1932885f47c8d4c88bb
Joshua <jwhite83193@gmail.com>
2025-06-26 10:08:06 -0500

    Schema & Commands Built

---
+++ sql-practice-1.sql
@@ -0,0 +1,38 @@
+INSERT INTO	users (username, firstName, lastName, age) VALUES 
+	('asmith', 'Alice', 'Smith', 20),
+	('bdoe', 'Bob', 'Doe', 33),
+	('cjohnson', 'Charlie', 'Johnson', 41),
+	('dlee', 'David', 'Lee', 19),
+	('ejones', 'Eve', 'Jones', 30),
+	('fkim', 'Frank', 'Kim', 15),
+	('gwhite', 'Grace', 'White', 22),
+	('hwang', 'Henry', 'Wang', 19),
+	('ijones', 'Ivy', 'Jones', 50),
+	('jdoe', 'John', 'Doe', 65),
+	('klee', 'Kevin', 'Lee', 62),
+	('lchen', 'Linda', 'Chen', 24),
+	('mlee', 'Michael', 'Lee', 29),
+	('nkim', 'Nancy', 'Kim', 18),
+	('ojohnson', 'Oscar', 'Johnson', 40),
+	('pchen', 'Peter', 'Chen', 55),
+	('qwang', 'Quincy', 'Wang', 28),
+	('rjones', 'Rachel', 'Jones', 37),
+	('slee', 'Sam', 'Lee', 21),
+	('tkim', 'Tina', 'Kim', 26);
+
+SELECT * FROM users;
+
+UPDATE users SET firstName = 'Samantha' WHERE username = 'slee';
+UPDATE users SET username = 'HeWang' WHERE firstName = 'Henry';
+UPDATE users SET lastName = 'Wang!' WHERE lastName = 'Wang';
+
+delete from users where (username = 'jdoe' or username = 'fkim' );
+
+select concat(username, ': ', firstName, lastName) as fullUser from users;
+select username, age + 4 as ageIn2030 from users;
+
+select * from users where lastName = 'Wang' and (firstName = 'Henry' or firstName = 'Quincy');
+
+select * from users order by lastName asc limit 12 offset 2;
+
+SELECT * FROM users;
